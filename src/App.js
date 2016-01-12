15e32e6d7a035c221fee5be7e3bae6c5e2efa83a
Kevin Smith <ksmithbaylor@gmail.com>
2016-01-12 01:55:33 -0500

    Add material-ui, roboto font, and basic container around page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Paper from 'material-ui/lib/paper';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <p>hello world</p>
+      <div style={{ width: '960px', margin: '1em auto' }}>
+        <Paper zDepth={2} style={{ padding: '1em' }}>
+          <p>hello world</p>
+        </Paper>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
+
+// Needed for material-ui
+injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('root'));
