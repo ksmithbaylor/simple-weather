87746c4758d7ba25c39dc0623ee2035aff55f2f8
Kevin Smith <ksmithbaylor@gmail.com>
2016-01-12 02:33:47 -0500

    Add ZipCodeEntry component and display list of zips

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import Paper from 'material-ui/lib/paper';
+
+import ZipCodeEntry from './ZipCodeEntry';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNewZip = this.handleNewZip.bind(this);
+    this.state = {
+      zips: []
+    };
+  }
+
   render() {
     return (
       <div style={{ padding: '1em' }}>
-        <Paper zDepth={2} style={{ padding: '1em' }}>
-          <p>hello world</p>
-        </Paper>
+        <ZipCodeEntry newZip={this.handleNewZip} />
+        {this.state.zips.map((z, i) => <p key={i}>{z}</p>)}
       </div>
     );
+  }
+
+  handleNewZip(zip) {
+    if (/\d{5}/.test(zip)) {
+      this.setState({
+        zips: this.state.zips.concat(zip)
+      });
+    } else {
+      alert("That's not a valid zip code!");
+    }
   }
 }
---
+++ src/ZipCodeEntry.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TextField from 'material-ui/lib/text-field';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleEnter = this.handleEnter.bind(this);
+  }
+
+  render() {
+    return (
+      <TextField
+        floatingLabelText="Enter a zip code"
+        onEnterKeyDown={this.handleEnter}
+      />
+    );
+  }
+
+  handleEnter(event) {
+    this.props.newZip(event.target.value);
+    event.target.value = '';
+  }
+}
