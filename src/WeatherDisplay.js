54185169b90cebcea6173fbfa7ec4c417e227fd5
Kevin Smith <ksmithbaylor@gmail.com>
2016-01-12 02:44:32 -0500

    Print to console when WeatherDisplay mounts

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Paper from 'material-ui/lib/paper';
 
 export default class WeatherDisplay extends React.Component {
+  componentDidMount() {
+    console.log('new zip:', this.props.zip)
+  }
+
   render() {
     return (
       <Paper style={{ padding: '1em', marginTop: '1em' }}>
