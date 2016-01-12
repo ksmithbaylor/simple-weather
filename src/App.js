ce744350c318d9749f85b39b3e351bed4f277491
Kevin Smith <ksmithbaylor@gmail.com>
2016-01-12 18:11:11 -0500

    Remove app for rebuilding

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
 import React from 'react';
 
-import ZipCodeEntry from './ZipCodeEntry';
-import WeatherDisplay from './WeatherDisplay';
-
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleNewZip = this.handleNewZip.bind(this);
-    this.state = {
-      zips: []
-    };
-  }
-
   render() {
-    const displays = this.state.zips.map((zip, i) => (
-      <WeatherDisplay key={i} zip={zip} />
-    )).reverse();
-
-    return (
-      <div style={{ padding: '1em' }}>
-        <ZipCodeEntry newZip={this.handleNewZip} />
-        {displays}
-      </div>
-    );
-  }
-
-  handleNewZip(zip) {
-    if (/\d{5}/.test(zip)) {
-      this.setState({
-        zips: this.state.zips.concat(zip)
-      });
-    } else {
-      alert("That's not a valid zip code!");
-    }
+    return <p>hello world</p>;
   }
 }
