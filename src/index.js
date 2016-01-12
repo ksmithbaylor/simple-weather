5abee921afb3979d921c194e3fb5ad50969c7908
Kevin Smith <ksmithbaylor@gmail.com>
2016-01-12 03:36:27 -0500

    Remove react-tap-event-plugin, not needed here

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
-
-// Needed for material-ui
-injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('root'));
