af780d5a721eaecb830d32ba0d961ec42794eb2e
Kevin Smith <ksmithbaylor@gmail.com>
2016-01-12 02:14:30 -0500

    Make container more appropriate for small screen

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Paper from 'material-ui/lib/paper';
 export default class App extends React.Component {
   render() {
     return (
-      <div style={{ width: '960px', margin: '1em auto' }}>
+      <div style={{ padding: '1em' }}>
         <Paper zDepth={2} style={{ padding: '1em' }}>
           <p>hello world</p>
         </Paper>
