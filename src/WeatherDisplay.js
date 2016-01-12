f83317d7df905b7833d3c718522362301546542f
Kevin Smith <ksmithbaylor@gmail.com>
2016-01-12 03:19:44 -0500

    Display some weather data

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -6,6 +6,12 @@ const baseUrl = 'http://api.openweathermap.org';
 const path = 'data/2.5/weather';
 const apiKey = 'f51233d6e43d44de92d140c73527c9bd';
 
+const columnStyle = {
+  width: '33%',
+  verticalAlign: 'middle',
+  display: 'inline-block'
+};
+
 export default class WeatherDisplay extends React.Component {
   constructor(props) {
     super(props);
@@ -16,26 +22,39 @@ export default class WeatherDisplay extends React.Component {
   }
 
   componentDidMount() {
-    const url = `${baseUrl}/${path}?zip=${this.props.zip}&APPID=${apiKey}`;
+    const url = `${baseUrl}/${path}?zip=${this.props.zip}&units=imperial&APPID=${apiKey}`;
 
     request.get(url).end((err, response) => {
       this.setState({
         loaded: true,
-        data: response
+        data: response.body
       });
     });
   }
 
   render() {
-    if (!this.state.loaded) {
-      return <p>Loading...</p>;
-    }
+    const contents = this.state.loaded ? (
+      <div style={{ verticalAlign: 'top' }}>
+        <div style={columnStyle}>
+          <h2>{this.state.data.name}</h2>
+          <p>{this.props.zip}</p>
+        </div>
+        <div style={columnStyle}>
+          <h1>{this.state.data.main.temp}&deg;</h1>
+        </div>
+        <div style={columnStyle}>
+          <p>{this.state.data.weather[0].description}</p>
+          <p>Humidity: {this.state.data.main.humidity}</p>
+          <p>Wind Speed: {this.state.data.wind.speed}</p>
+        </div>
+      </div>
+    ) : <p>Loading...</p>;
 
     console.log(this.state.data);
 
     return (
       <Paper style={{ padding: '1em', marginTop: '1em' }}>
-        <p>{this.props.zip}</p>
+        {contents}
       </Paper>
     );
   }
